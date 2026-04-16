728c00e6529a29e07ab08d6e965f2bafd67d9147
Jakub Dostalek <dostalek.jakub09@gmail.com>
2026-04-16 14:59:16 +0200

    první

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,16 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+        if field in data:
+            return data[field]
+
+
 
 def main():
-    pass
+    print(read_data("sequential.json", "dna_sequence"))
 
 
 if __name__ == '__main__':
